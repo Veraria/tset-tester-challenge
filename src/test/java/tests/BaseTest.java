04cd5c50089595ac3cee409ca0eee0e659477d36
Veraria <vericapp@yahoo.com>
2022-09-07 10:47:42 +0200

    modified BaseTest

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,17 +1,11 @@
 package tests;
 
-import com.tset.testerchallenge.PriceComponentPage;
-import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
-
 import java.time.Duration;
 import java.util.Arrays;
 
@@ -39,7 +33,7 @@ public class BaseTest {
 
     @AfterTest
     public void tearDown() {
-      //  driver.quit();
+        driver.quit();
     }
 
 }
